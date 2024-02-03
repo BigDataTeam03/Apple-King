a9b632eef3e0c3a59efbd64ef04a4a3264bc2cb7
pulpilisory <pulpilisory@gmailcom>
2024-02-03 20:59:12 +0900

    2024.02.03  adminUpdate.js 주석정리

--- src/main/webapp/adminUpdate.js
+++ src/main/webapp/adminUpdate.js
@@ -1,29 +1,36 @@
 
 
-/*--------------------------------------
+/*----------------------------------------------------------
  * Description: DB 에서 불러온 상품들을 조회하고 검색함.
  * Author : PDG, KBS
  * Date : 2024.02.02
  * Warning : 
- * Update :
- * 		Update 2024.02.02 by PDG, KBS
- * 			1. 주석 달음. 
- * 			2.
- *-------------------------------------- 
+ * Update --------------------------------------------------
+ * <<2024.02.02 by PDG, KBS>>
+ *	1. 주석 달음.
+ 	2. 상품 조회 및 수정이 가능하게 함. 
+ * <<2024.02.03 by PDG
+ * 	1. 주석을 친절하게 바꿈. 
+ *----------------------------------------------------------
  */
 
 // 페이지 실행후 바로 상품 전체 조회
 window.onload = function() {
 	$.ajax({
-		//post 방식으로 보내서 정보를 숨긴다
+		
+		// post method server request
 		type: "POST",
-		//기능을 실행하는 Servlet 보낸다
+		
+		// target server page(Servlet) url
 		url: "aProductListServlet",
-		//name 값을 받는다
+		
+		// request data (JSON)
 		data: { name: "" },
-		//json 타입으로 받는다
+		
+		// response data type -> JSON
 		dataType :"json",
-		//연결 성공시 createTable 메소드로 보낸다
+		
+		// server response success  -> response(Json data)
 		success: function(response) {
 			createTable(response);
 		},	
@@ -32,40 +39,54 @@ window.onload = function() {
 };
  
 function createTable(data) {
-	//검색해온 데이터를 배열로 변환
+	
+	//검색해온 데이터(dtos -> json -> Array  변환)
 	dataReal 	= Array.from(data) 
-	let table 	= "<table border='1'>"
-	// 테이블 헤더 추가
-	table 		+= "<tr><th>상품코드</th><th>상품명</th><th>수량</th><th>원산지</th> "                +
-				   "<th>생산일자</th><th>무게</th><th>사이즈</th><th>상세 이미지</th><th>조회수</th>"    +
-				   "<th>상품 등록일</th><th>품종</th><th>상품 섬네일 이미지</th></tr>"
-	/* 데이터 행 추가 */
+	let table 	= 
+	"<table border='1'>"
+	
+	// table 변수에 헤더 추가
+	table 		+= 
+		"<tr>"+
+		"<th>상품코드			</th>" + // col1
+		"<th>상품명 			</th>" + // col2
+		"<th>수량				</th>" + // col3
+		"<th>원산지			</th>" + // col4
+	    "<th>생산일자			</th>" + // col5
+	    "<th>무게				</th>" + // col6
+	    "<th>사이즈			</th>" + // col7
+	    "<th>상세 이미지		</th>" + // col8
+	    "<th>조회수			</th>" + // col9
+	    "<th>상품 등록일		</th>" + // col10
+	    "<th>품종				</th>" + // col11
+	    "<th>상품 섬네일 이미지	</th>" + // col12
+	    "</tr>"
+	    
+	// insert data rows
 	for(let i=0; i<data.length; i++)  {
 		table += "<tr>" +
-		"<td id='" + data[i].product_name + "'>"+data[i].product_code +
-		// 클릭 이벤트를 추가한다. 
-		// <a> 는 anchor 라고 함. #은 하이퍼링크가 현재페이지에 머물도록 한다. 
-		// onclick 은 하이퍼링크를 클릭하면 함수가 실행이 되게 한다. 
-		"</td>" +"<td>" +
-		"<a href='#' onclick='handleClick("+i+")'>" + data[i].product_name + "</a>"+ "</td>" +
-		"<td>" + data[i].product_qty + "</td>" +
-		"<td>" + data[i].origin + "</td>" +
-		"<td>" + data[i].manufacture_date + "</td>" +
-		"<td>" + data[i].weigtht + "</td>" +
-		"<td>" + data[i].size + "</td>" +
-		"<td>" + data[i].detail_image_name + "</td>" +
-		"<td>" + data[i].view_count + "</td>" +
-		"<td>" + data[i].product_reg_dat + "</td>" +
-		"<td>" + data[i].kind + "</td>" +
-		"<td>" + data[i].product_image_names + "</td>" +
+		"<td id='" + data[i].product_name + "'>"+data[i].product_code +"</td>" + 						// col1
+		"<td>" +"<a href='#' onclick='handleClick("+i+")'>" + data[i].product_name + "</a>"+ "</td>" +	// col2  
+		"<td>" + data[i].product_qty  			+ "</td>" + // col3 
+		"<td>" + data[i].origin 				+ "</td>" +	// col4 
+		"<td>" + data[i].manufacture_date  		+ "</td>" + // col5 
+		"<td>" + data[i].weigtht  				+ "</td>" + // col6 
+		"<td>" + data[i].size  					+ "</td>" + // col7 
+		"<td>" + data[i].detail_image_name  	+ "</td>" + // col8 
+		"<td>" + data[i].view_count  			+ "</td>" + // col9 
+		"<td>" + data[i].product_reg_dat 		+ "</td>" + // col10 
+		"<td>" + data[i].kind  					+ "</td>" + // col11
+		"<td>" + data[i].product_image_names  	+ "</td>" + // col12
 		"</tr>"
 	}
 	
+	// table end
 	table += "</table>"
-	/* html에 있는 result에 table을 넣는다 */
+	
+	// html result <- table
 	$("#result").html(table);
 	
-	   // 데이터를 화면에 출력
+	// 데이터를 화면에 출력
     let dataOutput = "<ul>";
     for (let i = 0; i < data.length; i++) {
         dataOutput += "<li>" + data[i].product_code + 
@@ -80,37 +101,62 @@ function createTable(data) {
     $("#Print_code").html("");
 	
 //수정을 위해 상품 이름을 클릭하면 수정텍스트 창에 선택한 정보가 보여지게 하기위한 문	
-}	   
-function handleClick(index){
+}
+
+// 테이블에서 클릭된 상품 정보를 가져오는 함수
+function handleClick(index){ //index : table cell number
 	
-	//jsp 에서 받은 값을 연결
-	let product_code 	= document.getElementById("product_code")
-	let product_name 	= document.getElementById("product_name")
-	let product_qty 	= document.getElementById("product_qty")
-	let origin = document.getElementById("origin")
-	let manufacture_date = document.getElementById("manufacture_date")
-	let weight = document.getElementById("weight")
-	let size = document.getElementById("size")
-	let detail_image_name = document.getElementById("detail_image_name")
-	let view_count = document.getElementById("view_count")
-	let product_reg_date = document.getElementById("product_reg_date")
-	let kind = document.getElementById("kind")
-	let product_image_names = document.getElementById("product_image_names")
+	// adminMain.jsp 안의 테이블에서 마우스로 클릭된 상품의 코드에 대한 table cell id 로 값을 불러옴.
+	let product_code 			= document.getElementById("product_code")
+	let product_name 			= document.getElementById("product_name")
+	let product_qty 			= document.getElementById("product_qty")
+	let origin 					= document.getElementById("origin")
+	let manufacture_date 		= document.getElementById("manufacture_date")
+	let weight 					= document.getElementById("weight")
+	let size 					= document.getElementById("size")
+	let detail_image_name 		= document.getElementById("detail_image_name")
+	let view_count 				= document.getElementById("view_count")
+	let product_reg_date 		= document.getElementById("product_reg_date")
+	let kind 					= document.getElementById("kind")
+	let product_image_names 	= document.getElementById("product_image_names")
 	
 	//연결된 값을 데이터값에 집어넣기
-	product_code.value = dataReal[index].product_code
-	product_name.value = dataReal[index].product_name
-	product_qty.value = dataReal[index].product_qty
-	origin.value = dataReal[index].origin
-	manufacture_date.value = dataReal[index].manufacture_date
-	weight.value = dataReal[index].weight
-	size.value = dataReal[index].size
-	detail_image_name.value = dataReal[index].detail_image_name
-	view_count.value = dataReal[index].view_count
-	product_reg_date.value = dataReal[index].product_reg_date
-	kind.value = dataReal[index].kind
-	product_image_names.value = dataReal[index].product_image_names
-	
+	product_code.value 			= dataReal[index].product_code
+	product_name.value 			= dataReal[index].product_name
+	product_qty.value 			= dataReal[index].product_qty
+	origin.value 				= dataReal[index].origin
+	manufacture_date.value 		= dataReal[index].manufacture_date
+	weight.value 				= dataReal[index].weight
+	size.value 					= dataReal[index].size
+	detail_image_name.value 	= dataReal[index].detail_image_name
+	view_count.value 			= dataReal[index].view_count
+	product_reg_date.value 		= dataReal[index].product_reg_date
+	kind.value 					= dataReal[index].kind
+	product_image_names.value 	= dataReal[index].product_image_names
+	/* 
+		이해가 안갈것 같아서 설명합니다. 
+		data real 은 위에 함수에서 server 에서 받은 json 파일을 array 로 변환한 형태이다
+		그러므로 dataReal은 
+		[
+			{product_code : 1,
+			 product_name : "가사과",
+			 ~
+			 product_image_names : -
+			},
+			{product_code : 2,
+			 product_name : "나사과",
+			 ~
+			 product_image_names : -
+			},
+			....
+		]
+		의 형태일것이다. 그러므로 dataReal[0] 의 값은 가사과에 대한 정보를 가져오게된다.
+		이때 product_name.value = dataReal.product_name  의 의미는
+		product_name 은 현재 adminMain.jsp 에서id 가prodcut_name 인 input tag 를 가리킨다
+		그 인풋 태그의 값을 dataReal 의 첫번째 원소에 위치한 jason 에서 Key 가 prodcut_name 인 것의 value 를 가져와서
+		넣는 다는 뜻이다.    
+		
+	.*/ 
 	
 }
 $(document).ready(function() {
