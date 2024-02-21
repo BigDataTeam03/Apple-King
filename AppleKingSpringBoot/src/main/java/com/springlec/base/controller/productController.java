f7c6f362d647bded2b42a687d9c2e47066cffc0f
Sumin Kim <411sumin@gmail.com>
2024-02-21 20:03:12 +0900

    2024.02.21 pagination test

--- AppleKingSpringBoot/src/main/java/com/springlec/base/controller/productController.java
+++ AppleKingSpringBoot/src/main/java/com/springlec/base/controller/productController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.springlec.base.model.ProductListDto;
 import com.springlec.base.service.ProductListDaoService;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class productController {
 	/*--------------------------------------
@@ -27,13 +29,41 @@ public class productController {
 	ProductListDaoService service;
 	
 	@GetMapping("/testProductDisplay")
-	public String testProductDisplay(Model model) throws Exception{
+	public String testProductDisplay(HttpServletRequest request, Model model)
+			
+			throws Exception{
+		
 		int pcnt = service.productCntDao();
-		model.addAttribute("pcnt",pcnt);
+		if(pcnt !=0) {
+			String pageNum ="1";
+			if(request.getParameter("pageNum") != null) {
+				pageNum = request.getParameter("pageNum");
+			}
+			int currentPage= Integer.parseInt(pageNum);
+			int pageSize = 8;
+			// 해당 페이지에서 출력할 첫번째 상품의 순번 :ex) 3페이지에서 보여줄 상품의 첫번째 코드는 17
+			int startRow = (currentPage-1)*pageSize +1;
+			// total page number calc
+			int pageCount = pcnt / pageSize + (pcnt%pageSize ==0? 0:1);
+			// 한페이지에 보여줄 페이지 블럭 : 페이지버튼의 개수
+			int pageBlock = 2;
+				
+			int startPage = ((currentPage-1)/pageBlock)*pageBlock+1;
+			int endPage = startPage + pageBlock -1;
+			//End page can be greater than the total page. 
+			//Last page to equalize with the total page count. 
+	
+			model.addAttribute("pcnt",pcnt);
+			model.addAttribute("currentPage",currentPage);
+			model.addAttribute("pageSize",pageSize);
+			model.addAttribute("startRow",startRow);
+			model.addAttribute("pageCount",pageCount);
+			model.addAttribute("pageBlock",pageBlock);
+			model.addAttribute("startPage",startPage);
+			model.addAttribute("endPage",endPage);
+			
+		}// pcnt !=0 end
 		return "/ProductPart/testProductDisplay";
-	}
-	
-	
-	
+	}// testProductDisplay End
 	
 }
--- AppleKingSpringBoot/src/main/webapp/WEB-INF/views/ProductPart/testProductDisplay.jsp
+++ AppleKingSpringBoot/src/main/webapp/WEB-INF/views/ProductPart/testProductDisplay.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -8,6 +10,42 @@
 	</head>
 	<body>
 		상품 총개수 : ${pcnt}
+		<c:set var ="pageSize" value="8"/><br>
+		페이지 당 상품수 : ${pageSize}
+		currentPage: ${currentPage }<br>
+		startRow:	${startRow}<br>
+		pageCount:${pageCount}<br>
+		pageBlock:${pageBlock}<br>
+		startPage:${startPage}<br>
+		endPage:${endPage}<br>
+		
+		<c:if test="${endPage> pageCount}">
+			<c:set var  ="endPage" value ="pageCount"/>
+		</c:if>
+		
+		<!-- PREVIOUS button generation -->
+		<c:if test="${startPage > pageBlock}">
+			<a href ="cGoHome.do?pageNum=${startPage-pageBlock}">Prev</a>
+		</c:if>
+		
+		<!-- Page number button generation -->
+		<c:forEach begin ="${startPage}" end ="${endPage}" var= "i">
+			<a href="cGoHome.do?pageNum=${i}">${i} </a>
+		</c:forEach>
+
+		<!-- NEXT button generation -->
+		<c:if test="${endPage < pageCount}">
+			<a href ="cGoHome.do?pageNum=${startPage+pageBlock}">Next</a>
+		</c:if>
+			
+			
+			
+	
+		
+	
+			
+		
+		
 		
 	
 	</body>
