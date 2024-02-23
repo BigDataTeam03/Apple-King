fb06753e75c13ad1cef443662121278199034260
Kangbeoms <tvyy11054@gmail.com>
2024-02-23 17:21:52 +0900

    2024-02-23 어드민 상품목록 수정기능 완료

--- AppleKingSpringBoot/src/main/java/com/springlec/base/service/aProductListDaoService.java
+++ AppleKingSpringBoot/src/main/java/com/springlec/base/service/aProductListDaoService.java
@@ -16,6 +16,10 @@ public interface aProductListDaoService {
 	//  상품목록 전체를 보여주고 정렬, 검색 변수가 추가된 인터페이스
 	public List<ProductListDto> productlist(String product_name,
 			 String selected, String orderby ) throws Exception;
-	public void updateProduct()
+	// 선택 상품 업데이트하는 변수가 추가된 인터페이스
+	public void updateProduct(String product_name,String product_qty,String origin,
+			String manufacture_date,String weight,String size,
+			String detail_image_name,String view_count,String product_reg_date,
+			String kind,String product_image_names, String product_code) throws Exception;
 	
 }
--- AppleKingSpringBoot/src/main/java/com/springlec/base/service/aProductListServiceImpl.java
+++ AppleKingSpringBoot/src/main/java/com/springlec/base/service/aProductListServiceImpl.java
@@ -16,6 +16,7 @@ public class aProductListServiceImpl implements aProductListDaoService {
 	 * Date : 2024.02.23
 	 * Update : 2024.02.23 KBS 
 	 * 		1. 페이지 생성
+	 * 		2. 상품 리스트 출력, 업데이트 기능 완료
 	 *-------------------------------------- 
 	 */
 
@@ -26,5 +27,19 @@ public class aProductListServiceImpl implements aProductListDaoService {
 	public List<ProductListDto> productlist(String product_name, String selected, String orderby) throws Exception {
 		// TODO Auto-generated method stub
 		return dao.productlist(product_name, selected, orderby);
+	}
+
+	@Override
+	public void updateProduct(String product_name, String product_qty, String origin, String manufacture_date,
+			String weight, String size, String detail_image_name, String view_count, String product_reg_date,
+			String kind, String product_image_names, String product_code) throws Exception {
+		// TODO Auto-generated method stub
+		dao.updateProduct(product_name, product_qty, origin,
+						  manufacture_date, weight, size, 
+						  detail_image_name, view_count, 
+						  product_reg_date, kind, product_image_names, 
+						  product_code);
+		
+		
 	}
 }
