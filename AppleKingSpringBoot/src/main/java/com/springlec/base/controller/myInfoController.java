7aed97df5a5e5cae8bdab535c616c68cadfec30e
Sumin Kim <411sumin@gmail.com>
2024-02-28 21:06:45 +0900

    2024-02-28 회원정보- 비번 획인/수정기능 구현.

--- AppleKingSpringBoot/src/main/java/com/springlec/base/controller/myInfoController.java
+++ AppleKingSpringBoot/src/main/java/com/springlec/base/controller/myInfoController.java
@@ -7,12 +7,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.springlec.base.dao.MyInfoDao;
 import com.springlec.base.model.MyInfoDto;
 import com.springlec.base.service.MyInfoDaoService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
 
 @Controller
 public class myInfoController {
@@ -28,6 +33,11 @@ public class myInfoController {
      *   3. Controller 를 하나 사용하기 위해서 myPageController 에 "service2" 추가 --> 에러걸림.짜증남.  
      * Update : 2024.02.26 DK
   	 *   1. ModifyDao/DeactivateDao 를 사용하여 회원정보수정/회원탈퇴 기능 구현. 
+  	 * Update : 2024.02.28 DK
+  	 * 	 1. 서비스 레이어를 통해 데이터베이스에서 해당 사용자의 비밀번호를 가져옴
+	 *	 2.	클라이언트가 제공한 이전 비밀번호(oldPassword)와 데이터베이스에서 가져온 비밀번호를 비교
+	 *	 3.	비밀번호가 일치하면 "match"를 반환하고, 일치하지 않으면 "not match"를 반환함.
+	 *	 4. 서비스 레이어를 통해 데이터베이스에서 해당 사용자의 새로운 비밀번호를 업데이트.
 	 *-------------------------------------- 
 	 */
 
@@ -74,5 +84,44 @@ public class myInfoController {
 	    session.invalidate();
 	    return "UserCheckPart/login_view";
 	}
+	
+	@GetMapping("/passwordReset")
+	public String passwordReset () throws Exception{
+		return "/MyPagePart/passwordReset";
+	}
+	
+		
+	@PostMapping("/validateOldPassword")
+	@ResponseBody 
+	public String validateOldPassword(@RequestParam("oldPassword") String oldPassword, HttpSession session) {
+		System.out.println("기존비번 : " + oldPassword);
+	    String cust_id = (String) session.getAttribute("userId");
+	    System.out.println("유저아이디 : " + cust_id);
+	    try {
+	        // Retrieve the database password using the service layer method
+	        String dbPassword = service.getUserPwById(cust_id);
+	        System.out.println("DB에있는 비번 : " + dbPassword);
+	        // Compare the old password with the database password
+	        if (oldPassword.equals(dbPassword)) {
+	        	System.out.println("match");
+	            return "match";
+	        } else {
+	        	System.out.println("notmatch");
+	            return "not match";
+	        }
+	        
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "error";
+	    }
+	}
+	
+	@PostMapping("updatePassword")
+	public String updatePassword(@RequestParam("confirmPassword") String confirmPassword,HttpSession session) throws Exception {
+	    String cust_id = (String) session.getAttribute("userId");
+	    service.updatePassword( cust_id, confirmPassword);
+	    return "redirect:/myInfo";
+	}
+	
 	
 }//CONTROLLER END
--- AppleKingSpringBoot/src/main/java/com/springlec/base/service/MyInfoDaoService.java
+++ AppleKingSpringBoot/src/main/java/com/springlec/base/service/MyInfoDaoService.java
@@ -8,5 +8,8 @@ public interface MyInfoDaoService {
 	public List<MyInfoDto> MyInfoDao(String cust_id) throws Exception;
 	public boolean ModifyDao(String name, String cust_pw, String tel, String email, String address, String cust_id) throws Exception;
 	public void DeactivateDao(String deact_date,String cust_id) throws Exception;
+	String getUserPwById(String cust_id) throws Exception;
+	void updatePassword(String cust_id, String confirmPassword) throws Exception;
+
 }
          
